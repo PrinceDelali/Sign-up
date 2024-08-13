603dcf634865b81eb80b3766c696db3481216da4
Delali Prince <princedelali30@gmail.com>
2024-08-13 16:28:54 +0000

    Initial commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        EmployeeName: 'Prince Delali',
+        EmployeeNumber: 'princedelali30@gmail.com',
+        EmployeeStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        EmployeeName: 'CSS Full Course',
+        EmployeeNumber: '97245',
+        EmployeeStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        EmployeeName: 'Flex-Box Tutorial',
+        EmployeeNumber: '36452',
+        EmployeeStatus: 'Paid',
+        status: 'Active'
+    },
+]
